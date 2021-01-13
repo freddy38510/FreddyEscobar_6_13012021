52a78d4cda027439b0e693bc9aa2725ba1ef11ee
Freddy <freddyescobar@hotmail.fr>
2021-01-13 14:32:05 +0100

    feat: add HTTP request logger middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan = require('./config/morgan');
+const { authLimiter } = require('./middlewares/rateLimiter');
 
 const app = express();
 
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
 module.exports = app;
